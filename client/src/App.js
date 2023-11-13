c7299120156a1e827ab3b29f03d967d674f58550
Max Friedman <mr.awesome10000@gmail.com>
2023-11-12 21:33:29 -0800

    client file upload

--- client/src/App.js
+++ client/src/App.js
@@ -32,8 +32,6 @@ function Fields() {
         {Array(NUMBER_OF_INPUTS).fill(<InputField />)}
       </div>
     </div>
-
-
   )
 }
 
@@ -46,24 +44,50 @@ function FieldLabel({text}) {
 }
 
 
+
+
+function ModelPrediction({model}) {
+  return (
+    <details>
+      <summary>Model {model}</summary>
+      <span>*Prediction Report for model {model}*</span>
+  </details>
+  )
+}
+
+
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Fields/>
+        Genre Classification API
+        <form>
+          <h1>Upload Input Data</h1>
+          <input type="file" />
+          <button type="submit">Upload</button>
+      </form>
       </header>
+
+      <div className="body-div">
+      <div className="intruction-div floater">
+        This API interfaces with 3 classification models that predict genre based on
+        popularity, acousticness, danceability, duration_ms, energy, instrumentalness,
+        liveness, loudness, mode, speechiness, and valence.
+
+        Upload a CSV containing columns for the features to interact with the API.
+      </div>
+
+      <div className="predictions floater">
+        <ModelPrediction model="1"/>
+        <ModelPrediction model="2"/>
+        <ModelPrediction model="3"/>
+      </div>
+
+      </div>
+
     </div>
   );
 }
